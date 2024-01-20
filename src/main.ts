21455e1263bff2f027e612df2a40b1dcfce21246
Mel-TB <melinda.trami@live.fr>
2024-01-20 19:35:09 +0100

    feat(main):  Implement DependencyName and badge)

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import * as fs from "fs";
 import { readmeTemplate } from "./templates/readmeTemplate.js";
 import { promptUser } from "./prompts/userPrompts.js";
 import { generateReadme } from "./utils/generateReadme.js";
+import { getDependenciesInfo } from "./utils/packageUtils.js";
 
 async function main() {
   const userInput = await promptUser();
+  const { dependencyNames, dependencyBadges } = getDependenciesInfo();
+
+  userInput.dependencyBadges = dependencyBadges;
+  userInput.dependencyNames = dependencyNames;
 
   const template = readmeTemplate;
   const readmeContent = generateReadme(template, userInput);
